37c048978da0036bfcd4539df322df41943ad3f0
matyoo-timoti <timsmothy@gmail.com>
2021-10-24 16:50:15 +0800

    • Just removed some whitespaces.

--- src/com/yhtomiTwehttaM/Person.java
+++ src/com/yhtomiTwehttaM/Person.java
@@ -1,17 +1,10 @@
 package com.yhtomiTwehttaM;
 
 public class Person {
-    private String firstname;
-    private String lastname;
+    private final String firstname;
+    private final String lastname;
     private final int number;
-    private boolean isPriority;
-
-    public Person() {
-        this.firstname = "";
-        this.lastname = "";
-        this.number = 0;
-        this.isPriority = false;
-    }
+    private final boolean isPriority;
 
     public Person(String firstname, String lastname, boolean isPriority, int number) {
         this.firstname = setName(firstname);
@@ -24,29 +17,18 @@ public class Person {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = setName(lastname);
-    }
-
     public boolean isPriority() {
         return isPriority;
-    }
-
-    public void setPriority(boolean priority) {
-        isPriority = priority;
     }
 
     public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = setName(firstname);
-    }
-
     private String setName(String name) {
-        if (name.isEmpty())
+        if (name.isEmpty()) {
             name = "\"N/A\"";
+        }
         return StringFormatter.capitalizeWord(name);
     }
 
