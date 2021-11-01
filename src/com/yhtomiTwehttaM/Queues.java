d737e52f99d65fa5ce30ada2bdadb98493739b28
matyoo-timoti <timsmothy@gmail.com>
2021-11-01 16:41:32 +0800

    • Code Cleanup. • Removed redundant codes in altSort() method.

--- src/com/yhtomiTwehttaM/Queues.java
+++ src/com/yhtomiTwehttaM/Queues.java
@@ -1,6 +1,8 @@
 package com.yhtomiTwehttaM;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 
 public class Queues {
 
@@ -29,27 +31,15 @@ public class Queues {
                 temp1.add(p);
             } else temp2.add(p);
         }
-
 /*
        If both temp queues are not empty, adding the contents of
        both arrays on the final queue by alternation.
 */
-        for (int i = 0; i < temp1.size() + temp2.size() + 2; i++) {
+        for (int i = 0; i < temp1.size() + temp2.size() + 3; i++) {
             if (!temp1.isEmpty()) {
                 list.add(temp1.remove(0));
             }
             if (!temp2.isEmpty()) {
-                list.add(temp2.remove(0));
-            }
-        }
-/*
-        Checks if there are any remaining item on
-        either array and transfers it to the queue.
-*/
-        if (!temp1.isEmpty() || !temp2.isEmpty()) {
-            if (!(temp1.isEmpty())) {
-                list.add(temp1.remove(0));
-            } else {
                 list.add(temp2.remove(0));
             }
         }
